693626f131737f04175ceae0e2ded89106c25c9e
joseeandre <jose.andrenunes5@gmail.com>
2021-07-05 21:17:14 -0300

    navbar

--- front-bootstore/src/components/App.js
+++ front-bootstore/src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Homepage from "./Homepage/Homepage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact>
+          <Homepage></Homepage>
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ front-bootstore/src/components/Homepage/Homepage.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Navbar from "../Navbar/Navbar";
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
+
+export default function Homepage() {
+    return (
+        <>
+            <Navbar></Navbar>
+            <Container>
+                <ImageGallery
+                    items={images}
+                    showBullets={true}
+                    showIndex={true}
+                    showThumbnails={false}
+                    lazyLoad={true}
+                    showPlayButton={false}
+                    showFullscreenButton={false}
+                    autoPlay={true} />
+            </Container>
+        </>
+    );
+}
+
+const Container = styled.div`
+    min-height:100vh;
+    width: 100vw;
+
+    .gallery {
+        width: 100% !important;
+    }
+`;
+
+const images = [
+    {
+        original: 'https://picsum.photos/id/1018/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    },
+    {
+        original: 'https://picsum.photos/id/1015/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    },
+    {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+];
